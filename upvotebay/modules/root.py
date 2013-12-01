285eaaabed0d022f673e931ce7f0282788b3b363
Duy Tran <duy@strayharbor.com>
2013-12-01 01:24:20 -0800

    Displaying a sign in/sign out link in the navbar depending on whether the user is signed in or signed out

--- upvotebay/modules/root.py
+++ upvotebay/modules/root.py
@@ -22,22 +22,16 @@ blueprint = Blueprint('root',
 @blueprint.route('/')
 @reddit_client
 def index(reddit=None):
-    if not session.get('username'):
+    if not is_logged_in():
         return landing(reddit)
 
     return home(reddit)
 
 def landing(reddit):
-    config = flask.current_app.config
+    oauth_scopes = flask.current_app.config['REDDIT_OAUTH_SCOPES']
+    session['auth_key'], auth_url = generate_auth_key_and_auth_url(reddit,
+                                                                   oauth_scopes)
 
-    # Generate a unique key for the authorization URL. We'll check for a
-    # matching key in the oauth callback, to protect against cross-site request
-    # forgery.
-    auth_key = str(uuid.uuid4())
-    session['auth_key'] = auth_key
-    auth_url = reddit.get_authorize_url(auth_key,
-                                        scope=config['REDDIT_OAUTH_SCOPES'],
-                                        refreshable=True)
     return render_template('landing.html',
                            auth_url=auth_url)
 
@@ -48,7 +42,27 @@ def home(reddit):
 @blueprint.route('/u/<path:extra_path>')
 @reddit_client
 def user(extra_path=None, reddit=None):
-    return render_template('home.html')
+    auth_url = ''
+
+    if not is_logged_in():
+        oauth_scopes = flask.current_app.config['REDDIT_OAUTH_SCOPES']
+        session['auth_key'], auth_url = generate_auth_key_and_auth_url(reddit,
+                                                                       oauth_scopes)
+
+    return render_template('home.html',
+                           auth_url=auth_url)
+
+def generate_auth_key_and_auth_url(reddit, scopes, refreshable=True):
+    # Generate a unique key for the authorization URL. We'll check for a
+    # matching key in the oauth callback, to protect against cross-site request
+    # forgery.
+    auth_key = str(uuid.uuid4())
+
+    auth_url = reddit.get_authorize_url(auth_key,
+                                        scope=scopes,
+                                        refreshable=refreshable)
+
+    return auth_key, auth_url
 
 # Logging out via POST request only
 # http://stackoverflow.com/a/14587231
@@ -57,6 +71,9 @@ def logout():
     session.pop('username', None)
     session.pop('oauth_refresh_token', None)
     return redirect(url_for('root.index'))
+
+def is_logged_in():
+    return bool(session.get('username'))
 
 @blueprint.app_errorhandler(404)
 def error_404(error):
