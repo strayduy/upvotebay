6d5a7c50547915f6278d25b91fec36c834219822
Duy Tran <duy@strayharbor.com>
2013-12-16 23:50:51 -0800

    Refactoring create_app method

--- upvotebay/app.py
+++ upvotebay/app.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# Standard libs
-import os
-
 # Third party libs
 from flask import Flask
-from flask.ext.mako import MakoTemplates
-import praw
 
 def create_app(config_object):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    # Use Mako instead of Jinja
-    mako = MakoTemplates(app)
+    register_extensions(app)
+    register_blueprints(app)
 
-    # Register blueprints
+    return app
+
+def register_extensions(app):
+    from flask.ext.mako import MakoTemplates
+
+    # Use Mako instead of Jinja
+    MakoTemplates(app)
+
+def register_blueprints(app):
     from .modules import root
     from .modules import oauth
     from .modules import api
     app.register_blueprint(root.blueprint)
     app.register_blueprint(oauth.blueprint)
     app.register_blueprint(api.blueprint, url_prefix='/api')
-
-    return app
